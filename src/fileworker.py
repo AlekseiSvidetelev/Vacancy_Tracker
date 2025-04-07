49760c571d0acc0cba0f16308eed07f1da3ee267
Aleksei <svid.aleksei@ya.ru>
2025-04-07 16:53:34 +0300

    add and update test_fileworker.py and fileworker.py

--- src/fileworker.py
+++ src/fileworker.py
@@ -43,10 +43,10 @@ class JSONSaver(Saver):
 
     def __init__(self, file_name: str = None):
         super().__init__(file_name)
-        self._file_name = file_name if file_name else "vacancies.json"
+        self.file_name = file_name if file_name else "vacancies.json"
 
     def __repr__(self):
-        return self.file_name
+        return f"{self.file_name}"
 
     @property
     def file_name(self) -> str:
@@ -58,21 +58,18 @@ class JSONSaver(Saver):
             clean_word = re.sub(r'[<>:"/\\|?*]', "", new_file_name)
             replace_spase = clean_word.replace(" ", "_")
             parts_name = replace_spase.split(".")
-            if parts_name[-1:] == "json":
-                self._file_name = ".".join(parts_name)
-            else:
-                self._file_name = parts_name[0] + ".json"
+            self._file_name = parts_name[0] + ".json"
         except Exception as e:
             print(f"Ошибка {Exception}: {e}")
 
     def read_from_file(self, file_name: str) -> list[dict[str, Any]]:
         """Функция для чтения JSON файла"""
         try:
-            if not os.path.join(DATA_DIR, file_name):
-                return []
+            if not os.path.exists(os.path.join(DATA_DIR, self.file_name)):
+                with open(os.path.join(DATA_DIR, self.file_name), "w", encoding="utf-8") as f:
+                    json.dump([], f)
             with open(os.path.join(DATA_DIR, self.file_name), "r", encoding="utf-8") as f:
-                data = json.load(f)
-            return data
+                return json.load(f)
         except Exception as e:
             print(f"Ошибка при чтении файла {Exception}: {e} ")
             return []
@@ -84,6 +81,9 @@ class JSONSaver(Saver):
                 raise TypeError("В файл можно добавлять только объекты класса Vacancy или его наследников")
             old_vacancies = self.read_from_file(self.file_name)
             new_vacancy = vacancy.to_dict()
+            if old_vacancies == []:
+                with open(os.path.join(DATA_DIR, self.file_name), "w", encoding="utf-8") as f:
+                    json.dump(old_vacancies, f, ensure_ascii=False, indent=4)
             for old_vacancy in old_vacancies:
                 if old_vacancy["id"] == new_vacancy["id"]:
                     return
@@ -110,13 +110,15 @@ class JSONSaver(Saver):
 
 
 if __name__ == "__main__":
-    saver = JSONSaver("vac")
+    saver = JSONSaver("vac.json")
     print(saver)
-    saver.file_name = "file.txt"
-    print(saver.file_name)
-    saver.file_name = "filesefd.refl.erfe"
-    print(saver.file_name)
-    saver.file_name = "file.rtg"
-    print(saver.file_name)
-    saver.file_name = "file><jg"
-    print(saver.file_name)
+    # saver.file_name = "file.txt"
+    # print(saver.file_name)
+    # saver.file_name = "filesefd.refl.erfe"
+    # print(saver.file_name)
+    # saver.file_name = "file.rtg"
+    # print(saver.file_name)
+    # saver.file_name = "file><jg"
+    # print(saver.file_name)
+    # saver.file_name = "asd_asd.asd"
+    # print(saver.file_name)
--- tests/test_fileworker.py
+++ tests/test_fileworker.py
@@ -2,3 +2,41 @@ from src.fileworker import JSONSaver
 
 from src.vacancy_operations import Vacancy
 
+def test_class_jsonsaver(test_list_hh):
+    vacancy_list = Vacancy.cast_to_object_list(test_list_hh)
+    res_1 = JSONSaver("vacancies_test")
+    assert res_1.file_name == 'vacancies_test.json'
+    res_2 = JSONSaver("test")
+    assert res_2.file_name == "test.json"
+    assert repr(res_2) == "test.json"
+    for vacavcy in vacancy_list:
+        res_1.save_to_file(vacavcy)
+
+        # Проверка существования файла
+        # assert file_path.exists(), "Файл не был создан"
+
+        # # Проверка содержимого файла
+        # with open(file_path, 'r', encoding='utf-8') as f:
+        #     loaded_data = json.load(f)
+        #
+        # assert loaded_data == test_data, "Данные в файле не соответствуют ожидаемым"
+
+
+
+# def test_write_json_to_file(tmp_path: Path):
+#     # Подготовка тестовых данных
+#     test_data = {"name": "Alice", "age": 30, "hobbies": ["reading", "travel"]}
+#     file_path = tmp_path / "test_data.json"
+#
+#     # Вызов тестируемой функции
+#     JSONSaver.save_to_file(test_data, file_path)
+#
+#     # Проверка существования файла
+#     assert file_path.exists(), "Файл не был создан"
+#
+#     # Проверка содержимого файла
+#     with open(file_path, 'r', encoding='utf-8') as f:
+#         loaded_data = json.load(f)
+#
+#     assert loaded_data == test_data, "Данные в файле не соответствуют ожидаемым"
+
